f9c359a19acd44cc6172f6c2e1cfb03bfb6d8709
HuangLin <tomcat1969@gmail.com>
2018-12-19 17:00:45 -0800

    finish profile

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import '../styles/App.css';
 import  TopNavBar from "./TopNavBar";
 import Main from "./Main";
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNavBar/>
-        <Main/>
+          <Main/>
+
 
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
+import Profile from "./Profile";
+import nba from 'nba';
 
 export default class Main extends Component {
+    state = {
+        playerId:201939,
+        playerInfo:{},
+    }
+    componentDidMount(){
+        const player = nba.findPlayer('Stephen Curry');
+        console.log(player);
+
+        nba.stats.playerInfo({PlayerID: player.playerId})
+            .then(({commonPlayerInfo,playerHeadlineStats})=>{
+                const playerInfo = Object.assign({},commonPlayerInfo[0],playerHeadlineStats[0]);
+                this.setState({playerInfo})
+            })
+
+    }
+
+
+
+
+
     render(){
         return(
-            <div className="main">main</div>
+            <div className="main">
+              <Profile playerInfo={this.state.playerInfo}/>
+            </div>
         );
 
     }
